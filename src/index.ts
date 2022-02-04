4cc84acbbbbf830b9acc73154355a61aebbd5fbb
Karyna Kananovich <karyna.kananovich@gmail.com>
2022-02-04 13:32:06 +0300

    refactor: allocate enum MatchResult to a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import { CsvFileReader } from './CsvFileReader'
+import { MatchResult } from './MathResult'
 
 const reader = new CsvFileReader('football.csv')
 reader.read()
+console.log(reader.data)
 
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
 
 let manUnitedWins = 0
 
