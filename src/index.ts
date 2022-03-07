6b46097dc8db2821b011d9943e0c81ab7eb1597a
Karyna Kananovich <karyna.kananovich@gmail.com>
2022-03-07 14:53:04 +0300

    feat: use instance of HtmlReport class

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { MatchReader } from './MatchReader'
 import { WinsAnalysis } from './analyzers/WinsAnalysis'
 import { ConsoleReport } from './reportTargets/ConsoleReport'
 import { Summary } from './Summary'
+import { HtmlReport } from './reportTargets/HtmlReport'
 
 const csvFileReader = new CsvFileReader('football.csv')
 
 const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
-const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
+// const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport())
 summary.buildAndPrintReport(matchReader.matches)
