9da5db39e3fcb222a971a13c82a92f6a8e971a11
Karyna Kananovich <karyna.kananovich@gmail.com>
2022-03-09 10:22:19 +0300

    feat: create static methods and remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import { CsvFileReader } from './CsvFileReader'
 import { MatchReader } from './MatchReader'
-import { WinsAnalysis } from './analyzers/WinsAnalysis'
-import { ConsoleReport } from './reportTargets/ConsoleReport'
 import { Summary } from './Summary'
-import { HtmlReport } from './reportTargets/HtmlReport'
 
-const csvFileReader = new CsvFileReader('football.csv')
-
-const matchReader = new MatchReader(csvFileReader)
+const matchReader = MatchReader.fromCsv('football.csv')
 matchReader.load()
 
-// const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport())
+const summary = Summary.winsAnalysisWithHtmlReport('Man United')
 summary.buildAndPrintReport(matchReader.matches)
